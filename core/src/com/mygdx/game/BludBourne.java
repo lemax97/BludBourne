07f22b41e5b68ac1f70e3a2309b24c3dc1bd98a2
lemax97 <lemax97@mail.ru>
2022-06-20 16:19:36 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/BludBourne.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import com.badlogic.gdx.Game;
+
+public class BludBourne extends Game {
+
+	@Override
+	public void create() {
+
+	}
+}
